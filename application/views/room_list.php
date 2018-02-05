7dca9e7b3cff9eba192bf1fca5ec42de99a3a34c
iNoyoka <as1232161@gmail.com>
2018-02-06 07:32:29 +0800

    roomListCssUpdate

--- application/views/room_list.php
+++ application/views/room_list.php
@@ -1,18 +1,35 @@
-<table class="table table-striped result">
-	<thead>
-		<tr>
-		    <th>ID</th>
-		    <th>房間名稱</th>
-		    <th>在線人數</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach($data as $value){
-  				echo "<tr><td>".$value->{'room_id'}
-  					."</td><td>".anchor('chat/?hash='.$value->{'room_hash'},$value->{'room_name'})
-  					."</td><td>2</td></tr>";
-		}
-		?>
-	</tbody>
-</table>
-<a href="room/add/">新增房間</a>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MoonTalk</title>
+	<link rel="stylesheet" type="text/css" href="../../css/room_list.css">
+</head>
+<body>
+	<div class="showRoomListBlock">
+		<div class="roomListBar">
+			<h1>ID</h1>
+			<h1>會議室名稱</h1>
+			<h1>人數限制</h1>
+		</div>
+		<div class="showRoomList">
+			<div class="roomTable">						
+				<?php foreach ($data as $value) {
+					echo "<div class="list"><div class="item1"><h1>".$value->{'room_id'}
+			  					."</h1></div><div class="item2"><h1>".anchor('chat/?hash='.$value->{'room_hash'},$value->{'room_name'})
+			  					."</h1></div><div class="item3"><h1>2</h1></div></div>";
+				}
+				?>	
+			</div>			
+		</div>
+		<div class="addRoomBar">
+			<a href="room/add/" class="roomAdd">
+				<img src="../../img_source/roomList/roomAdd.png" alt="roomAdd">
+				<h1>新增會議室</h1>
+			</a>
+		</div>
+	</div>
+	<div class="userInfo"></div>
+	<div class="notice"></div>
+</body>
+</html>
+
