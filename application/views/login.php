5d26e10cd67842227a13ba3b6679ac55f9e1969c
iNoyoka <as1232161@gmail.com>
2018-02-05 17:49:20 +0800

    updateLoginPage

--- application/views/login.php
+++ application/views/login.php
@@ -2,36 +2,30 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>OOXX.Talk登入</title>
+    <title>MoonTalk</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url().'css/style.default.css';?>" type="text/css" />
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    <script type="text/javascript" src=" http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js"></script></head>
+    <script type="text/javascript" src=" http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js"></script>
 
-<body class="loginpage">
-<div class="loginbox">
-    <div class="loginboxinner">
-
-        <div class="logo">
-            <h1 class="logo">OOXX.<span>Talk</span></h1>
-        </div><!--logo-->
-        <br clear="all" /><br />
-
-        <form id="login" action="<?php echo site_url().'login/check';?>" method="post">
-
-            <div class="username">
-                <div class="usernameinner">
-                    <input type="text" name="user" id="user" placeholder="名稱"/>
-                </div>
-            </div>
-
-            <input type="hidden" name="head" value="1"> <!-- 頭像先藏起來-->
-
-            <button id="login_btn">登入</button>
-        </form>
-
-    </div><!--loginboxinner-->
-</div><!--loginbox-->
-
+    <link rel="stylesheet" type="text/css" href="../../css/login/login.css">
+</head>
+<body onload="document.getElementById('user').focus();">
+    <div id="body">
+        <div class="loginlogo">
+            <img src="../../img_source/login/moonTalkLogo.png" alt="LCS_Logo">
+        </div>
+        <div class="loginbox">
+            <div class="user">
+                <form name="user" action="<?php echo site_url().'/login/check';?>" onsubmit="" method="post">
+                    <hc>USERNAME:</hc>
+                    <input type="text" name="user" id="user">
+                    <input type="hidden" name="head" value="1"> <!-- 頭像先藏起來-->
+                    <input type="submit" name="enter" value="加入會議室">
+                </form>
+            </div>                       
+        </div>       
+    </div>
+    
 </body>
 </html>
