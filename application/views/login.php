da9070d99bd2a3cb34f90ef9a11de3a5b613038c
iNoyoka <as1232161@gmail.com>
2018-02-08 15:45:59 +0800

    addJqueryToLogin

--- application/views/login.php
+++ application/views/login.php
@@ -1,18 +1,51 @@
 <html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>MoonTalk</title>
-    <link rel="stylesheet" type="text/css" href="../../css/login.css">
-</head>
-<body onload="document.getElementById('user').focus();">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>MoonTalk</title>        
+        <link rel="stylesheet" type="text/css" href="../../css/login.css">        
 
-    <div id="body">
+        <!--jQuery link-->     
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
+        <script type="text/javascript" src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+        
+        <link type="text/css" rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+        
+        <script>
+           $(function() { 
+                $("#setIcon").dialog({ 
+                    autoOpen: false, 
+                    modal: true,
+                    show: null, 
+                    hide: null, 
+                    buttons: { 
+                        "Ok": function() { $(this).dialog("close"); }, 
+                        "Cancel": function() { $(this).dialog("close"); }
+                    }
+                }); 
+                $("#showUserIcon").click(function() { 
+                    $("#setIcon").dialog("open"); 
+                    return false; 
+                }); 
+            });
+
+
+        </script>
+    </head>
+
+    <body onload="document.getElementById('user').focus();">                
         <div class="loginlogo">
             <img src="../../img_source/login/moonTalkLogo.png" alt="LCS_Logo">
-        </div>       
-        
+        </div>               
         <div class="loginbox">
+            <div id="settingUserIcon">   
+                <div id="setIcon" title="請選擇你想要的頭像">
+                    <img src="../../img_source/login/img_1.jpg">
+                    <img src="../../img_source/login/img_2.jpg">
+                    <img src="../../img_source/login/img_3.jpg">
+                </div>
+                <img id="showUserIcon" src="../../img_source/login/img_1.jpg">                
+            </div>
             <div class="user">
                 <form name="user" action="<?php echo site_url().'/login/check';?>" onsubmit="" method="post">
                     <hc>USERNAME:</hc>
@@ -21,49 +54,6 @@
                     <input type="submit" name="enter" value="加入會議室">
                 </form>
             </div>                       
-        </div>       
-    </div>
-    
-    <div style="margin-left:auto;margin-right:auto;width:425px;height:240px;">
-            <div style="float: left;">
-                <div id="1" class="picture" style="display: block">
-                    <img src="../../img_source/login/img_1_wide.jpg">
-                </div>
-                <div id="2" class="picture" style="display: none">
-                    <img src="../../img_source/login/img_2_wide.jpg">
-                </div>
-                <div id="3" class="picture" style="display: none">
-                    <img src="../../img_source/login/img_3_wide.jpg">
-                </div>          
-            </div>
-
-            <table style="float: right;">
-                <tr>
-                    <th>
-                        <a href="javascript:void(0)" onclick="openImg('1');">
-                        <img src="../../img_source/login/img_1.jpg"></a>
-                    </th>
-                    <th>
-                        <a href="javascript:void(0)" onclick="openImg('2');">
-                        <img src="../../img_source/login/img_2.jpg"></a>
-                    </th>
-                    <th>
-                        <a href="javascript:void(0)" onclick="openImg('3');">
-                        <img src="../../img_source/login/img_3.jpg"></a>
-                    </th>
-                </tr>
-            </table>
-        </div>
-<script>
-function openImg(imgName) {
-  var i, x;
-  x = document.getElementsByClassName("picture");
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  document.getElementById(imgName).style.display = "block";
-}
-</script>
-
-</body>
+        </div>           
+    </body>
 </html>
